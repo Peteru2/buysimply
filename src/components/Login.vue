d891169a5dd46d8a8d771c329dbca209bbcb62a8
Olatunji Peter <polalekan526@gmail.com>
2026-02-18 12:55:45 +0100

    Team Achieve login Page

---
+++ src/components/Login.vue
@@ -0,0 +1,138 @@
+<template>
+  <div class="flex h-screen">
+
+    <!-- LEFT SECTION -->
+    <div class="hidden md:flex w-1/2 bg-purple-100 flex-col justify-center p-12">
+
+      <!-- Logo -->
+      <div class="flex items-center gap-2 mb-2">
+        <img 
+          src="/logo.png"
+          alt="Logo"
+          class="w-40 h-40"
+        />
+       
+      </div>
+
+      <!-- Side Image -->
+      <img 
+        src="/sideImg.png"
+        alt="Team Illustration"
+        class="rounded-xl shadow-lg"
+      />
+
+      <h2 class="text-[#61227D] text-center font-semibold text-lg my-2">
+        Team Achieve
+      </h2>
+
+      <p class="text-gray-600 text-center">
+        Your perfect solution for funding your desires
+      </p>
+    </div>
+
+
+    <!-- RIGHT SECTION -->
+    <div class="w-full md:w-1/2 flex items-center justify-center px-4">
+
+      <div class="w-full    md:px-10 px-2 ">
+ <div class="flex md:hidden justify-center  items-center ">
+        <img 
+          src="/logo.png"
+          alt="Logo"
+          class="w-40 h-40"
+        />
+       
+      </div>
+        <h2 class="text-2xl text-center md:text-left font-bold text-[#61227D] mb-2">
+          Welcome Back
+        </h2>
+
+        <p class="text-gray-500 text-center md:text-left mb-6 text-sm">
+          Enter your email address and password to access your account.
+        </p>
+
+        <!-- Email -->
+        <div class="mb-4">
+          <label class="block text-sm text-[#3E3E3E]
+ font-medium mb-1">
+            Email Address <span class="text-red-500">*</span>
+          </label>
+          <input
+            type="email"
+            v-model="email"
+            placeholder="Enter your email"
+            class="w-full border border-[#C6C6C6] rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-[#61227D]"
+          />
+        </div>
+
+        <!-- Password -->
+        <div class="mb-4">
+          <label class="block text-sm text-[#3E3E3E] font-medium mb-1">
+            Password <span class="text-red-500">*</span>
+          </label>
+          <div class="w-full flex border border-[#C6C6C6] rounded-lg  focus:outline-none focus:ring-2 focus:ring-[#61227D]">
+          <input
+            type="password"
+            v-model="password"
+            placeholder="Enter your password"
+            class="w-full px-3 py-2 border-0 outline-none"
+          />
+          <div class="ml-auto border-l border-l-[#C6C6C6] bg-gray-100
+  w-10 justify-center rounded-tr-lg rounded-br-lg items-center flex">
+          e
+          </div>
+          </div>
+        </div>
+
+        <!-- Remember + Forgot -->
+        <div class="flex justify-between items-center mb-6 text-sm">
+          <label class="flex items-center text-[#3E3E3E] gap-2">
+            <input type="checkbox" v-model="remember" />
+            Remember me
+          </label>
+
+          <a href="#" class="text-[#61227D] hover:underline">
+            Forgot Password?
+          </a>
+        </div>
+
+        <!-- Button -->
+        <button
+          @click="login"
+          class="w-full bg-[#61227D] text-white py-2 rounded-lg hover:opacity-90 transition"
+        >
+          Sign in
+        </button>
+
+        <!-- Bottom Text -->
+        <p class="text-sm text-center mt-4">
+          Don’t have an account?
+          <span class="text-[#61227D] font-semibold cursor-pointer">
+            Sign up
+          </span>
+        </p>
+
+      </div>
+
+    </div>
+
+  </div>
+</template>
+
+
+<script setup>
+import { ref } from "vue"
+
+const email = ref("")
+const password = ref("")
+const remember = ref(false)
+
+const login = () => {
+  if (!email.value || !password.value) {
+    alert("Please fill in all fields")
+    return
+  }
+
+  alert("Login successful!")
+}
+</script>
